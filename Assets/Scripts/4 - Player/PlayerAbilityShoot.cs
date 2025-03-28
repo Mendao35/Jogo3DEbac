15470588c14c55fa6de8b1e29f87a484edb17ae2
Antonio <mendao_22@hotmail.com>
2025-03-28 14:20:11 -0300

    Modulo 30

--- Assets/Scripts/4 - Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/4 - Player/PlayerAbilityShoot.cs
@@ -5,18 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
- 
 
-    public GunBase gunBase;
+
+    public GunBase gunBase1;
+    public GunBase gunBase2;
+
     public Transform gunPosition;
 
-    private GunBase _currentGun;
+    private GunBase _currentGun1;
+    private GunBase _currentGun2;
+
+    private int _IndexGun;
 
     protected override void Init()
     {
         base.Init();
 
-        CreateGun();
+        CreateGun1();
+        CreateGun2();
+        _IndexGun = 1;
 
         if (Input.GetKeyDown(KeyCode.F))
         {
@@ -30,26 +37,53 @@ public class PlayerAbilityShoot : PlayerAbilityBase
         //inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
     }  
 
-    private void CreateGun()
+    private void CreateGun1()
     {
        
-        _currentGun = Instantiate(gunBase, gunPosition);
+        _currentGun1 = Instantiate(gunBase1, gunPosition);
 
-        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        _currentGun1.transform.localPosition = _currentGun1.transform.localEulerAngles = Vector3.zero;
+        Debug.Log("GunBase Instanciada");
+    }
+
+    private void CreateGun2()
+    {
+
+        _currentGun2 = Instantiate(gunBase2, gunPosition);
+
+        _currentGun2.transform.localPosition = _currentGun2.transform.localEulerAngles = Vector3.zero;
         Debug.Log("GunBase Instanciada");
     }
 
     private void StartShoot()
     {
-        _currentGun.StartShoot();
-        Debug.Log("Start Shoot");
+        if(_IndexGun == 1)
+        {
+            _currentGun1.StartShoot();
+            Debug.Log("Start Shoot");
+        }
+        if (_IndexGun == 2)
+        {
+            _currentGun2.StartShoot();
+            Debug.Log("Start Shoot");
+        }
+
     }
 
     private void CancelShoot()
     {
-        _currentGun.StopShoot();
-        Debug.Log("Cancel Shoot");
+        if(_IndexGun == 1)
+        {
+            _currentGun1.StopShoot();
+            Debug.Log("Cancel Shoot");
+        }
+        if (_IndexGun == 2)
+        {
+            _currentGun2.StopShoot();
+            Debug.Log("Cancel Shoot");
+        }
     }
+        
 
     private void Update()
     {
@@ -61,5 +95,15 @@ public class PlayerAbilityShoot : PlayerAbilityBase
         {
             CancelShoot();
         }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _IndexGun = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _IndexGun = 2;
+        }
+
+
     }
 }
